21db6ed17c5e48d5e6314b568437ff862c4d7aa1
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-03-06 16:58:40 -0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -4,11 +4,14 @@ import pandas as pd
 import pickle
 import TextTokenizer as prtxt
 import numpy as np
+from langdetect import detect
 
-
-model = pickle.load(open("model/model_hate.pkl", 'rb'))
+model_pt = pickle.load(open("model/model_hate_pt.pkl", 'rb'))
+model_en = pickle.load(open("model/model_hate_en.pkl", 'rb'))
+model_sp = pickle.load(open("model/model_hate_sp.pkl", 'rb'))
 
 app = Flask(__name__)
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -20,28 +23,40 @@ def predict():
         else:
             df_raw = pd.DataFrame(test_json, columns=test_json[0].keys())
 
+    application_data = df_raw
 
-    proc = prtxt.TextProcessor()
+    application_text = application_data['frase']
+    lang = detect(application_text)
+    proc = prtxt.TextProcessor(lang)
 
-    aplicationData = df_raw
+    vectorization_text_application = proc.process(application_text)
 
-    aplicationText = aplicationData['frase']
-    vectorizationTextApplication = proc.process(aplicationText)
+    for i in range(2714 - len(vectorization_text_application[0])):
+        vectorization_text_application[0].append('0')
 
-    for i in range(2714 - len(vectorizationTextApplication[0])):
-        vectorizationTextApplication[0].append('0')
-
-    XAPP = np.array(vectorizationTextApplication)
+    XAPP = np.array(vectorization_text_application)
     results = []
-    for i in range(len(model)):
-        results.append(model[i].predict(XAPP))
+    if lang == 'pt':
+        for i in range(len(model_pt)):
+            results.append(model_pt[i].predict(XAPP))
+    elif lang == 'en':
+        for i in range(len(model_en)):
+            results.append(model_en[i].predict(XAPP))
+    elif lang == 'sp':
+        for i in range(len(model_sp)):
+            results.append(model_sp[i].predict(XAPP))
+    else:
+        for i in range(len(model_en)):
+            results.append(model_en[i].predict(XAPP))
 
-    df_raw['valor'] = 0
+
+    df_raw['valor'] = 1
     for i in range(len(results)):
         if results[i] == 'yes':
             df_raw['valor'] = 1
 
     return df_raw.to_json(orient='records')
+
 
 if __name__ == '__main__':
     port = os.environ.get('PORT', 5000)
