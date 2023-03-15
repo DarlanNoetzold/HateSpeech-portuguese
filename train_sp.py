32b94904e120cdc858cf9e04bf19fbda543d0e50
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-03-15 20:12:44 -0300

    Update train_sp.py

--- train_sp.py
+++ train_sp.py
@@ -63,7 +63,7 @@ pickle.dump(classifiers, open('model/model_hate_sp.pkl', 'wb'))
 for i in range(len(metrics)):
     res_mets = []
     for chave, valor in results_tests_metrics.items():
-        res_mets.append(valor['test_' + metrics[i]])
+        res_mets.append(valor['sp_test_' + metrics[i]])
     grf.mostrarGraficoLinhas(res_mets[0], res_mets[1], res_mets[2], kfolds,
                              [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1], "Número do 'fold' (n)", label_metrics[i])
 
